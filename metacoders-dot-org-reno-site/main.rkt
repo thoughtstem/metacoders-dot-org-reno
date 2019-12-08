1ffda9408c33ef066d96b6b59f89f592a209fc6e
Lindsey Handley, Ph.D <lindseyhandley@gmail.com>
2019-12-08 11:50:41 -0800

    Update main.rkt

--- metacoders-dot-org-reno-site/main.rkt
+++ metacoders-dot-org-reno-site/main.rkt
@@ -25,6 +25,7 @@
 (define (index)
   (page index.html
         (city-page
+         #:city-name "Reno"
          #:banner-url "https://live.staticflickr.com/2916/14377709314_0ccfdd06cf_k.jpg" ;"https://live.staticflickr.com/4414/36082399434_18e1e4bdff_o.jpg"
          #:locations-list (list
                            (location-courses
